2a96a30d370aa2688ea593dddbc87ac75221543b
siavasham <66677857+siavasham@users.noreply.github.com>
2021-03-13 21:06:26 +0330

    ok

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,49 +2,49 @@
     <div class="container section-padding-y">
     <div class="row">
         <div class="col-lg-3 mb-4 mb-lg-0"><img class="mb-4" src="img/logo.png" alt="" width="110">
-        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+        <p class="text-muted">{{ __('home.about-us') }}</p>
         </div>
         <div class="col-lg-2 mb-4 mb-lg-0">
-        <h2 class="h5 mb-4">Quick Links</h2>
+        <h2 class="h5 mb-4"></h2>
         <div class="d-flex">
-            <ul class="list-unstyled d-inline-block mr-4 mb-0">
+            <!-- <ul class="list-unstyled d-inline-block mr-4 mb-0">
             <li class="mb-2"><a class="footer-link" href="#">History </a></li>
             <li class="mb-2"><a class="footer-link" href="#">About us</a></li>
             <li class="mb-2"><a class="footer-link" href="#">Contact us</a></li>
             <li class="mb-2"><a class="footer-link" href="#">Services</a></li>
-            </ul>
+            </ul> -->
         </div>
         </div>
         <div class="col-lg-2 mb-4 mb-lg-0">
-        <h2 class="h5 mb-4">Services</h2>
+        <h2 class="h5 mb-4"></h2>
         <div class="d-flex">
-            <ul class="list-unstyled mr-4 mb-0">
+            <!-- <ul class="list-unstyled mr-4 mb-0">
             <li class="mb-2"><a class="footer-link" href="#">History </a></li>
             <li class="mb-2"><a class="footer-link" href="#">About us</a></li>
             <li class="mb-2"><a class="footer-link" href="#">Contact us</a></li>
             <li class="mb-2"><a class="footer-link" href="#">Services</a></li>
-            </ul>
+            </ul> -->
         </div>
         </div>
         <div class="col-lg-5">
-        <h2 class="h5 mb-4">Contact Info</h2>
+        <h2 class="h5 mb-4">{{ __('home.contact-us') }}</h2>
         <ul class="list-unstyled mr-4 mb-3">
-            <li class="mb-2 text-muted">728  Ocello Street, San Diego, California. </li>
-            <li class="mb-2"><a class="footer-link" href="tel:619-851-4132">619-851-4132</a></li>
-            <li class="mb-2"><a class="footer-link" href="mailto:Nova@example.com">Nova@example.com</a></li>
+            <li class="mb-2 text-muted">ایران، تهران </li>
+            <!-- <li class="mb-2"><a class="footer-link" href="tel:619-851-4132">619-851-4132</a></li> -->
+            <li class="mb-2"><a class="footer-link" href="mailto:support@stronghold.live">support@stronghold.live</a></li>
         </ul>
         <ul class="list-inline mb-0">
-            <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-google-plus"></i></a></li>
-            <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-instagram"></i></a></li>
+            <!-- <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-facebook-f"></i></a></li> -->
+            <!-- <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-twitter"></i></a></li> -->
+            <!-- <li class="list-inline-item"><a class="social-link" href="#"><i class="fab fa-google-plus"></i></a></li> -->
+            <li class="list-inline-item"><a class="social-link" href="instagram.com/stronghold.live"><i class="fab fa-instagram"></i></a></li>
         </ul>
         </div>
     </div>
     </div>
     <div class="copyrights">       
     <div class="container text-center py-4">
-        <p class="mb-0 text-muted text-sm">&copy; 2019, Your company. Template by <a href="https://bootstraptemple.com" class="text-reset">Bootstrap Temple</a>.</p>
+        <p class="mb-0 text-muted text-sm">&copy; 2021, StrongHold.</p>
     </div>
     </div>
 </footer>
