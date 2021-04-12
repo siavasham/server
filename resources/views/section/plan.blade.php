b03e0cafdc1b700bca756df18313bbdbfb53e8d6
siavash younesi <unesi.s@dpi>
2021-04-12 16:45:26 +0430

    ok

--- resources/views/section/plan.blade.php
+++ resources/views/section/plan.blade.php
@@ -3,10 +3,10 @@
         <h2 class="mb-5">{{ __('home.plans') }}</h2>
         <div class="row justify-content-center">
         @foreach ($plans as $plan)
-                <div class="card card-pricing text-center mb-4 mx-3 px-3 border-0 shadow ">
+                <div class="card card-pricing text-center mb-4 mx-3 px-4 border-0 shadow ">
                     <span class=" w-60 mx-auto px-4 py-1 rounded-bottom bg-primary text-white">{{ __('home.'.$plan->type) }}</span>
                     <div class="bg-transparent card-header pt-4 border-0">
-                        <h1 class="h4 font-weight-normal text-success text-center" >%<span class="price mr-2">{{$plan->profit}}</span></h1>
+                        <h1 class="h3 font-weight-normal text-success text-center" >%<span class="price mr-2">{{$plan->profit}}</span></h1>
                     </div>
                     <div class="card-body pt-0">
                         <ul class="list-unstyled mb-4">
